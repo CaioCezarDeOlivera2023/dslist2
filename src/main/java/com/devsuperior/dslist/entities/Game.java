54c1320eabbafbbea7fcd68c71a84daaf7a73410
Caio Cezar <caio_25m@hotmail.com>
2024-11-12 11:47:43 -0300

    GameSeed

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -22,21 +22,25 @@ public class Game {
 	private Integer year;
 	private String genre;
 	private String plataforms;
+	private Double score;
 	private String imgUrl;
 	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
 	public Game() {
 		
 	}
 
-	public Game(Long id, String title, Integer year, String genre, String plataforms, String imgUrl,
+	public Game(Long id, String title, Integer year, String genre, String plataforms,Double score, String imgUrl,
 			String shortDescription, String longDescription) {
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
 		this.plataforms = plataforms;
+		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
 		this.longDescription = longDescription;
@@ -80,6 +84,14 @@ public class Game {
 
 	public void setPlataforms(String plataforms) {
 		this.plataforms = plataforms;
+	}
+
+	public Double getScore() {
+		return score;
+	}
+
+	public void setScore(Double score) {
+		this.score = score;
 	}
 
 	public String getImgUrl() {
